3b102a6aef34a330d37a75626cbcb18527b85495
Tatyana Asriyan <tasriyan@hotmail.com>
2025-08-10 17:04:51 -0500

    Added dependency injection project

---
+++ Propel.FeatureFlags.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Propel.FeatureFlags.Cache;
+using Propel.FeatureFlags.Client;
+using Propel.FeatureFlags.Core;
+
+namespace Propel.FeatureFlags.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddFeatureFlags(this IServiceCollection services, FlagOptions options)
+	{
+		// Register core services
+		services.AddSingleton<IFeatureFlagEvaluator, FeatureFlagEvaluator>();
+		services.AddSingleton<IFeatureFlagClient, FeatureFlagClient>();
+
+		// Register chain builder and context evaluator
+		services.AddSingleton(_ => EvaluatorChainBuilder.BuildChain());
+
+		if (options.UseCache == true && string.IsNullOrEmpty(options.RedisConnectionString))
+			services.AddSingleton<IFeatureFlagCache, MemoryFeatureFlagCache>();
+
+		return services;
+	}
+
+}
