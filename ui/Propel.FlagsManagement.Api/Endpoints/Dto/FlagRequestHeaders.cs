dd3edf9981ad8b6dac80aa5c279c0dc49008d216
Tatyana Asriyan <tasriyan@hotmail.com>
2025-09-18 21:47:02 -0500

    separated bounded contexts: moved flag maagement to its own repository

--- ui/Propel.FlagsManagement.Api/Endpoints/Dto/FlagRequestHeaders.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Propel.FlagsManagement.Api.Endpoints.Dto;
-
-public record FlagRequestHeaders(
-		[FromHeader(Name = "X-Scope")] string Scope,
-		[FromHeader(Name = "X-Application-Name")] string? ApplicationName,
-		[FromHeader(Name = "X-Application-Version")] string? ApplicationVersion
-	);
--- ui/Propel.FlagsManagement.Api/Endpoints/Shared/BusinessValidatationResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Propel.FlagsManagement.Api.Endpoints.Shared;
-
-public record BusinessValidationResult
-{
-	public bool IsValid { get; init; }
-	public Dictionary<string, string[]> Errors { get; init; } = [];
-}
