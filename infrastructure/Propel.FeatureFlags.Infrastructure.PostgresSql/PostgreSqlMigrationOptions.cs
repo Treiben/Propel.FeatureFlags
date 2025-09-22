28788ff3b83e913ba09fcf91f54f57c3dcf29fb4
Tatyana Asriyan <tasriyan@hotmail.com>
2025-09-22 17:20:51 -0500

    Cleaned up solution: removed dead files, prepared projects for packaging.

--- infrastructure/Propel.FeatureFlags.Infrastructure.PostgresSql/PostgreSqlMigrationOptions.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Propel.FeatureFlags.Infrastructure.PostgresSql;
-
-public record PostgreSqlMigrationOptions(string Connection, string Schema = "public", string MigrationTable = "flags_schema_migrations", string Database = "propel_feature_flags");
